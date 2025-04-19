b045556549194909d88aeee17ec492ec09e9226d
Saleh <salehalshumer@gmail.com>
2025-04-19 23:04:30 +0300

    saleh

--- my_app/src/SignUp.js
+++ my_app/src/SignUp.js
@@ -77,49 +77,29 @@ const SignUp = () => {
           <div className="form-group">
             <label htmlFor="name">Full Name </label>
             <input
-              type="text" id="name" name="name" value={formData.name} onChange={handleChange} className={errors.name ? 'error' : ''}/>
+              type="text" id="name" name="name"  onChange={handleChange} className={errors.name ? 'error' : ''}/>
             {errors.name && <span className="error-text">{errors.name}</span>}
           </div>
           
           <div className="form-group">
             <label htmlFor="email">Email </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className={errors.email ? 'error' : ''}
-            />
+            <input type="email" id="email" name="email" onChange={handleChange} className={errors.email ? 'error' : ''}/>
             {errors.email && <span className="error-text">{errors.email}</span>}
           </div>
           
           <div className="form-group">
             <label htmlFor="password">Password </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className={errors.password ? 'error' : ''}
-            />
+            <input type="password" id="password" name="password" onChange={handleChange} className={errors.password ? 'error' : ''}/>
             {errors.password && <span className="error-text">{errors.password}</span>}
           </div>
           
           <div className="form-group">
             <label htmlFor="confirmPassword">Confirm Password </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              className={errors.confirmPassword ? 'error' : ''}
+            <input type="password" id="confirmPassword"name="confirmPassword" onChange={handleChange} className={errors.confirmPassword ? 'error' : ''}
             />
             {errors.confirmPassword && <span className="error-text">{errors.confirmPassword}</span>}
           </div>
-          <Lottie type= "submit" onClick={handleSubmit} style ={{width:"350px"}} animationData={Ani5} loop autoplay /> 
+          <Lottie type= "submit" onClick={handleSubmit} style ={{width:"500px", margin:"auto",position: 'absolute',transform: 'translate(-15%,-31%)' }} animationData={Ani5} loop autoplay /> 
         </form>
         
         <div className="signup-footer">
