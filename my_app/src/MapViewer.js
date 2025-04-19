c1dfd9ac80415bedc3af0050ab0762c13f11c55d
Almoghairi <abdulazizalmoghairi@outlook.com>
2025-04-19 20:59:34 +0300

    Map fixed

--- my_app/src/MapViewer.js
+++ my_app/src/MapViewer.js
@@ -8,7 +8,7 @@ import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 function Map() {
   const [map] = useState('/pictures/map.png');
   const gameImage = ['/pictures/forgotten.png', '/pictures/tampet.png','/pictures/inferno.png','/pictures/cryzone.png', '/pictures/pharoah.png'];
-  const posX = ['30%', '25%','75%','70%','35%']; // x-axis as percentages
+  const posX = ['30%', '20%','80%','77%','35%']; // x-axis as percentages
   const posY = ['20%', '50%', '70%', '15%', '72%']; // y-axis also as percentages
   const gameTitle = ['The Forgotten Asylum', 'The Tempest’s Wrath','Inferno Spiral','CryzoneX','Pharaoh’s Curse'];
   const gameDiscription = [
@@ -26,17 +26,21 @@ function Map() {
     <motion.div 
     initial={{ opacity: 0, y: 20 }}
     animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: .8, ease: 'easeOut' }}
+    transition={{ duration: 0.8, ease: 'easeOut' }}
     >
-        <Container className="vhd-100 d-flex justify-content-center">
+        <Container className="vhd-100 mt-5 d-flex justify-content-center">
         <div style={{ 
-          
-          position: 'relative', 
-          width: '100%', 
-          maxWidth: '1200px', 
+          position: 'relative',
+          width: '100%',
+          maxWidth: '1200px',
           minWidth: '600px',
-          minHeight: '600px',
-          aspectRatio: '16/9' 
+          aspectRatio: '16 / 9',
+          borderRadius: '24px',
+          overflow: 'hidden', // cut it off the image is larger than the widthXheight
+          boxShadow: '0 12px 30px rgba(0, 0, 0, 0.3)', 
+          backdropFilter: 'blur(10px)', // blur
+          background: 'rgba(255, 255, 255, 0.05)', // light frosted glass
+          border: '1px solid rgba(255, 255, 255, 0.1)', // subtle outline
         }}>
           <img
             src={map}
@@ -44,7 +48,7 @@ function Map() {
             style={{
               width: '100%',
               height: '100%',
-              objectFit: 'contain',
+              objectFit: 'fill',
               display: 'block',
             }}
           />
@@ -74,9 +78,9 @@ function Map() {
               key={activeIndex}
               style={{
                 position: 'absolute',
-                top: posY[activeIndex],
-                left: posX[activeIndex],
-                transform: 'translate(-50%, 0)',
+                top: '30%',
+                left: '40%',
+                transform: 'translate(-50%, -50%)', // center the imageCard
                 zIndex: 999,
               }}
               initial={{ opacity: 0, y: 20 }}
