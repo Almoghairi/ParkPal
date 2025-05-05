1bba1a12b3a52f6d1654dfa4f572b7b37ae36cbd
Almoghairi <abdulazizalmoghairi@outlook.com>
2025-05-05 22:14:13 +0300

    try

--- backend/routes/VQServer.js
+++ backend/routes/VQServer.js
@@ -29,7 +29,7 @@ router.post('/join', async (req, res) => {
     if (existing) return res.status(201).json({ error: 'You are already in queue' });
     
 
-    const estimatedWaitInMs = (queueLength + 1) * 5 * 60 * 1000; // +1 for this visitor
+    const estimatedWaitInMs = (queueLength) * 5 * 60 * 1000; // +1 for this visitor
 
     const entry = new VQSchema({
       gameName,
